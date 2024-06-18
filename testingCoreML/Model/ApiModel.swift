dcf56f08ebb438504d11564e13f3635d31f7e761
Kirill Sklyarov <k.sklyarov@yandex.ru>
2024-06-18 11:17:32 +0300

    - Сделан парсинг с википедии.

---
+++ testingCoreML/Model/ApiModel.swift
@@ -0,0 +1,8 @@
+//
+//  ApiModel.swift
+//  testingCoreML
+//
+//  Created by Kirill Sklyarov on 18.06.2024.
+//
+
+import Foundation
---
+++ testingCoreML/Services/NetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkService.swift
+//  testingCoreML
+//
+//  Created by Kirill Sklyarov on 18.06.2024.
+//
+
+import Foundation
